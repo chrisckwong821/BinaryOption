4c75dd16fb14e19b114a5a6ea470b21f5e7972e2
Chris WONG <chrisckwong821@gmail.com>
2020-11-16 18:59:15 +0800

    Update BinaryOptionInterface.sol

--- BinaryOptionInterface.sol
+++ BinaryOptionInterface.sol
@@ -4,4 +4,5 @@ interface BinaryOptionInterface {
 
   function IsExercised(uint ID) external view returns (bool);
   function IsExpiried(uint ID) external view returns (bool);
+  function IsBought(uint ID) external view returns (bool);
 }

1998cc8877f23d0a449ed7b270b9ea41e4dc51c4
Chris WONG <chrisckwong821@gmail.com>
2020-11-16 17:13:33 +0800

    Update BinaryOptionInterface.sol

--- BinaryOptionInterface.sol
+++ BinaryOptionInterface.sol
@@ -2,8 +2,5 @@ pragma solidity ^0.6.7;
 
 interface BinaryOptionInterface {
 
-  // getRoundData and latestRoundData should both raise "No data present"
-  // if they do not have data to report, instead of returning unset values
-  // which could be misinterpreted as actual reported values.
   function IsExercised(uint ID) external view returns (bool);
 }

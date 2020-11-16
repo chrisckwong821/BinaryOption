fe9bac312d9db4e58a35fd9e0d8a8344a42a071b
Chris WONG <chrisckwong821@gmail.com>
2020-11-16 19:50:56 +0800

    Update OptionOverOption.sol

--- OptionOverOption.sol
+++ OptionOverOption.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.6.7;
 
+import ".//BinaryOption.sol";
 import ".//BinaryOptionInterface.sol";
-//import "https://github.com/chrisckwong821/BinaryOption/blob/main/BinaryOptionInterface.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/math/SafeMath.sol";
 
 contract OptionOfOption {

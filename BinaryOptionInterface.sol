aa99ac6837b3be210198aa85904ad1a0b24cc0bd
Chris WONG <chrisckwong821@gmail.com>
2020-11-16 17:27:57 +0800

    Update BinaryOptionInterface.sol

--- BinaryOptionInterface.sol
+++ BinaryOptionInterface.sol
@@ -3,5 +3,5 @@ pragma solidity ^0.6.7;
 interface BinaryOptionInterface {
 
   function IsExercised(uint ID) external view returns (bool);
-  function IsExpiry(uint ID) external view returns (bool);
+  function IsExpiried(uint ID) external view returns (bool);
 }
